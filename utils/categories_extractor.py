8ccc29c5b131031c7f03aaf68866b6a89a455aaf
Renzo Rico <renzorico10@gmail.com>
2024-02-25 13:27:51 +0100

    Added execution permissions to main.py, created a crontab and improved categories_extractor

--- utils/categories_extractor.py
+++ utils/categories_extractor.py
@@ -4,11 +4,11 @@ from .driver import get_headless_driver
 def get_valid_category_numbers():
     category_numbers = []
     driver = get_headless_driver()
-    # for start_num in range(1, 1001, 200):
-    #     end_num = min(start_num + 199, 1000)
+    for start_num in range(1, 1001, 200):
+        end_num = min(start_num + 199, 1000)
     # Just for testing purposes
-    for start_num in range(1, 50, 10):
-        end_num = min(start_num + 10, 50)
+    # for start_num in range(1, 50, 10):
+    #     end_num = min(start_num + 10, 50)
         for num in range(start_num, end_num + 1):
             url = f"https://tienda.mercadona.es/categories/{num}"
             driver.get(url)
